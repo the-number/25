d3d4b99f816264ce475424fbb4ea6642b46eea2a
Gunter Liszewski <gunter@m2.bteco.ltd.uk>
2018-10-11 10:06:19 +0100

    to resolve: # ewline is rejected as token
    or some other ill
    
    Signed-off-by: Gunter Liszewski <gunter@m2.bteco.ltd.uk>

--- 25/bison-rpcalc.scm
+++ 25/bison-rpcalc.scm
@@ -23,7 +23,7 @@
            (<input> <line>))
 
 (<line>    (#\newline)
-           (<exp> #\newline) : (format #t "~a~%" $1)
+           (<exp> #\newline) : (format #t "~a~%" $1))
 
 (<exp>     (NUM)             : $1
            (<exp> <exp> #\+) : (+ $1 $2)
