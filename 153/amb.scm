8b223ff473e79cdc229b22e18b47867eea0167c7
Gunter Liszewski <gunter@m2.bteco.ltd.uk>
2018-10-15 11:21:36 +0100

    unknown
    Signed-off-by: Gunter Liszewski <gunter@m2.bteco.ltd.uk>

---
+++ 153/amb.scm
@@ -0,0 +1,20 @@
+#! /usr/bin/env guile
+  amb
+!#
+ (define (*failure*) (error "Failure"))
+
+ (define-syntax amb
+  (syntax-rules ()
+  ((amb) (*failure*))
+  ((amb ?x) ?x)
+  ((amb ?x ?y)
+  (let ((old-failure *failure*))
+  ((call-with-current-continuation
+    (lambda (cc)
+    (set! *failure* 
+      (lambda () 
+      (set! *failure* old-failure)
+      (cc (lambda () ?y))))
+    (lambda () ?x))))))
+  ((amb ?x ?rest ...)
+  (amb ?x (amb ?rest ...)))))
