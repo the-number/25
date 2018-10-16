2b805718ef22ec4fe85fba3d69056cd0a59f58d1
Gunter Liszewski (s70-11) <gunter.liszewski@gmail.com>
2018-10-16 22:47:14 +0100

    abc: another view on amb, with call/cc

---
+++ 153/abc.scm
@@ -0,0 +1,25 @@
+#! /usr/bin/env guile
+  amb
+  from SICP amb, require
+  2018 (c) Gunter Liszewski
+!#
+
+(define *f* #f)
+
+(define-syntax abc
+  (syntax-rules ()
+    ((abc) (*f*))
+    ((abc ?a) ?a)
+    ((abc ?a ?b)
+      (let ((c *f*))
+        ((call/cc 
+           (lambda (d)
+             (set! *f*
+               (lambda ()
+                 (set! *f* c)
+                 (d (lambda () ?b))))
+             (lambda () ?a))))))
+    ((abc ?a ?e ...)
+      (abc ?a (abc ?e ...)))))
+
+(list (abc 'noun 'hauptwort 'nomen) (abc 'verb 'tatwort 'the-verb))
