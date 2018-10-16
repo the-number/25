3104428b70429a07f4f2f65c4b8617f4c84db78b
Gunter Liszewski (s70-11) <gunter.liszewski@gmail.com>
2018-10-17 00:54:55 +0100

    abc

--- 153/abc.scm
+++ 153/abc.scm
@@ -1,5 +1,5 @@
 #! /usr/bin/env guile
-  amb
+  abc, amb
   from SICP amb, require
   2018 (c) Gunter Liszewski
 !#
@@ -19,7 +19,38 @@
                  (set! *f* c)
                  (d (lambda () ?b))))
              (lambda () ?a))))))
-    ((abc ?a ?e ...)
-      (abc ?a (abc ?e ...)))))
+    ((abc ?a ?rest ...)
+      (abc ?a (abc ?rest ...)))))
 
-(list (abc 'noun 'hauptwort 'nomen) (abc 'verb 'tatwort 'the-verb))
+
+(define (require a)
+  (if (not a) (abc)))
+
+(define *un* '())
+(define (parse input)
+  (set! *un* input)
+    (let ((a (parse-sentence)))
+      (require (null? *un*))
+      a))
+
+(define (parse-word a)
+  (require (not (null? *un*)))
+  (require (memq (car *un*) (cdr a)))
+  (let ((b (car *un*)))
+    (set! *un* (cdr *un*))
+    (list (car a) (b))))
+
+(define (parse-noun-phrase)
+  (list 'noun-phrase (parse-word articles) (parse-word nouns)))
+
+(define (parse-sentence)
+  (list 'sentence (parse-noun-phrase) (parse-word verbs)))
+
+;;; words
+
+(define nouns
+  '(noun dog cat man hut chair))
+(define verbs
+  '(verb sits lies wals is hides))
+(define articles
+  '(article the a))
