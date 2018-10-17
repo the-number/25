21dec2ae541006d5ad2b8d257b7521344d8a5e2a
Gunter Liszewski <gunter@m2.bteco.ltd.uk>
2018-10-17 15:26:19 +0100

    amb: in C, from c2.org
    Signed-off-by: Gunter Liszewski <gunter@m2.bteco.ltd.uk>

---
+++ amb-c.c
@@ -0,0 +1,37 @@
+/* amb-c.c
+  2018 (c) Gunter Liszewski
+  from wiki.c2.org */
+
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define AMB_FAILURE 42
+
+
+
+int
+amb (void)
+{
+  pid_t a = fork ();
+  if (a == -1) { perror ("amb"); exit (1);}
+  if (a == )   { return 1;}
+  else
+  {
+    int b;
+    pid_t c;
+    do
+    {
+      c = waitpid (a, &b, 0);
+    } while ((c == -1) && (errno == EINTR));
+    if (c == -1) { perror ("amb"); exit (1);}
+    if (WIFEXITED (b))
+    {
+      if (WEXITSTATUS (b) == AMB_FAILED) { return 0;}
+      else { _exit (WEXITSTATUS (b);}
+    }
+    _exit (1);
+  }
+}
