7c256902716779b7e53226e01e1ed9f22ca5e1f8
Gunter Liszewski <gunter@m2.bteco.ltd.uk>
2018-12-12 17:59:25 +0000

    number+lalr.scm: syntactic category of binary positional positive numerals

---
+++ 25/number+lalr.scm
@@ -0,0 +1,69 @@
+#! /usr/bin/env guile
+
+  number-lalr
+
+  2018 (c) Gunter Liszewski
+
+  Of the introduction of the semantics paper
+    Scott and Strachey
+!#
+
+(use-modules (system base lalr))
+
+(define lambda-parser
+  (lalr-parser
+
+;;; --- token
+
+(#{\xa;}# *1 *0)
+
+;;; --- grammar
+
+(<input>         ()
+                 (<input> <line>))
+
+(<line>          (#{\xa;}#)
+                 (<nu> #{\xa;}#)   : (format #t "~a~%" "ok")
+                 (error #{\xa;}#)  : (list 'recover $1 $2))
+
+(<nu>            (*1)
+                 (<nu> *0)
+                 (<nu> *1))))
+
+;;; --- lexer
+
+(define (make-lexer e)
+  (lambda ()
+    (define (a b c)
+      (cond ((eof-object? b) '*eoi*)              ; eof
+            ((or (char=? b #\space) (char=? b #\tab))
+             '*space)                             ; space
+            ((char=? b #\0)
+             '*0)                                 ; 0
+            ((char=? b #\1)
+             '*1)                                 ; 1
+            ((char=? b #\newline)
+             '#{\xa;}#)                           ; new line
+            (else
+             (e "SCAN error : " b)
+	     (a (read-char) #\space))))
+    (a (read-char) #\space)))
+
+;;; --- errors
+
+(define e
+  (lambda (a . b)
+    (define c (lambda (a) (display a (current-error-port))))
+    (c a)
+    (for-each c b)
+    (newline)))
+
+;;; --- control
+
+(define start
+  (lambda ()
+    (drain-input (current-input-port))
+    (lambda-parser (make-lexer e) e)))
+
+(start)
+
